ae1024eb5bf87f2235b273f2ed85a70fcbc1f00d
xian <zhagndongxian@star-media.cn>
2020-09-29 19:01:38 +0800

    修改Demo演示

--- app/src/main/java/com/star/content/GameCenterActivity.kt
+++ app/src/main/java/com/star/content/GameCenterActivity.kt
@@ -1,81 +1,27 @@
 package com.star.content
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentPagerAdapter
-import com.google.android.material.tabs.TabLayout
-import com.star.content.fragment.GameCenterFragment
-import com.star.content.fragment.TestFragment
+import android.widget.Toast
+import com.starmedia.tinysdk.StarMedia
 import kotlinx.android.synthetic.main.activity_game_center.*
 
 class GameCenterActivity : BaseActivity() {
 
-    val tabs = mapOf(
-            Pair("娱乐", 1001),
-            Pair("体育", 1002),
-            Pair("小游戏", 1003),
-            Pair("IT", 1004),
-            Pair("手机", 1005),
-            Pair("财经", 1006),
-            Pair("汽车", 1007),
-            Pair("房产", 1008),
-            Pair("时尚", 1009),
-            Pair("文化", 1011),
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_center)
 
-        tabs.forEach {
-            val newTab = tl_game_center.newTab()
-            newTab.text = it.key
-            newTab.tag = it.value
-            tl_game_center.addTab(newTab)
-        }
 
-        tl_game_center.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabReselected(tab: TabLayout.Tab) {
-
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab) {
-
-            }
-
-            override fun onTabSelected(tab: TabLayout.Tab) {
-                vp_game_center.currentItem = tab.position
-            }
-        })
-
-        vp_game_center.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tl_game_center))
-
-        vp_game_center.adapter = object : FragmentPagerAdapter(
-                supportFragmentManager,
-                BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
-        ) {
-            override fun getItem(position: Int): Fragment {
-
-                return if (position == 2) {
-                    GameCenterFragment()
-                } else {
-                    val fragment = TestFragment()
-
-                    val bundle = Bundle()
-
-                    bundle.putString("Title", tabs.entries.toList()[position].key)
-
-                    fragment.arguments = bundle
-
-                    fragment
+        btn_smallgame.setOnClickListener {
+            StarMedia.openStarGame(this) {
+                if (!it) {
+                    Toast.makeText(this, "开发游戏失败", Toast.LENGTH_LONG).show()
                 }
             }
-
-
-            override fun getCount(): Int {
-                return tabs.size
-            }
         }
+
+
 
     }
 }
